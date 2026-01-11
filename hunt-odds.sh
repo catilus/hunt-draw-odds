7e6bdc5414fc272d4036a52ef9ecf6ef43f3b5a2
catilus <helene.m.piet@gmail.com>
2026-01-11 13:36:58 -0700

    Added a conditional check if PDF file already exists.

--- hunt-odds.sh
+++ hunt-odds.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 # Fetch PDF of odds from AZGFD for: 2025, elk. Save into a PDF file.
-curl https://azgfd-portal-wordpress-pantheon.s3.us-west-2.amazonaws.com/wp-content/uploads/2025/08/22144539/2025-AZ-Elk-Draw-Odds.pdf > elk_odds_2025.pdf
+# Don't fetch if file already exists at location.
+if [ -f "elk_odds_2025.pdf" ];
+then
+	echo "File already downloaded."
+else
+	curl https://azgfd-portal-wordpress-pantheon.s3.us-west-2.amazonaws.com/wp-content/uploads/2025/08/22144539/2025-AZ-Elk-Draw-Odds.pdf > elk_odds_2025.pdf
+fi
 
 # Display odds based on game unit
-echo "Pick a game unit [e.g. '04B']: "
-read game_unit
-echo "Draw odds for GMU $game_unit: \n"
-less elk_odds_2025.pdf | grep $game_unit 
+read -p "Pick a game unit [e.g. '04B']: " game_unit
+echo "Draw odds for GMU $game_unit: "
+echo "=============================="
+less elk_odds_2025.pdf | grep -i $game_unit 
