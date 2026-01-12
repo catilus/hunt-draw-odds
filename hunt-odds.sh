b776fe3af482bde3a47e0d060f559b915a901103
catilus <helene.m.piet@gmail.com>
2026-01-11 20:51:17 -0700

    Returns odds for GMU and species based on user input.

--- hunt-odds.sh
+++ hunt-odds.sh
@@ -11,6 +11,10 @@ fi
 
 # Display odds based on game unit
 read -p "Pick a game unit [e.g. '04B']: " game_unit
-echo "Draw odds for GMU $game_unit: "
+read -p "Pick a species [Bull or Antlerless]: " species
+
+echo "Draw odds for GMU $game_unit and $species elk: "
 echo "=============================="
-less elk_odds_2025.pdf | grep -i $game_unit 
+echo "UNIT	HUNT	YEAR	METHOD	SEASON OPEN	SEASON CLOSE	SPECIES	1st CHOICE	2nd CHOICE	PERMITS	ODDS"	# display header
+less elk_odds_2025.pdf | grep -i $game_unit | grep -i $species
+# less elk_odds_2025.pdf | grep -iP "$game_unit|$species" 
